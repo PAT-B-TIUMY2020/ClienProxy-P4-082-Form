5fff39d70db5a65bf80004f0a6760ead8a8ca9d9
UB8082 <lucky.ahmad.ft18@mail.umy.ac.id>
2020-10-27 18:14:00 +0700

    Add project files.

---
+++ ClienProxy-P4-082-Form/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienProxy_P4_082_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ServiceReference1.MatematikaClient obj = new
+            ServiceReference1.MatematikaClient();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+
+            ServiceReference1.Koordinat a = new
+           ServiceReference1.Koordinat();
+            ServiceReference1.Koordinat b = new
+           ServiceReference1.Koordinat();
+            a.X = 7;
+            a.Y = 8;
+
+            b.X = 5;
+            b.Y = 6;
+            double selisihX = a.X - b.X;
+            double selisihY = a.Y - b.Y;
+            double jarak = Math.Sqrt(Math.Pow(selisihX, 2) +
+           Math.Pow(selisihY, 2));
+            Console.WriteLine("Hasil Koordinat " + jarak);
+            Console.ReadLine();
+
+            label1.Text = "1 + 2 = " + hasilTambah;
+
+            label2.Text = "3 - 2 = " + hasilKurang;
+
+            label3.Text = "2 x 2 = " + hasilKali;
+
+            label4.Text = "2 : 2 = " + hasilBagi;
+
+            label5.Text = "Hasil Koordinat" + jarak;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
